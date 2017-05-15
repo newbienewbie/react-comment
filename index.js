55ea5bd1c87de6e2ad42510137498955b0e57357
newbienewbie <webbot@sina.com>
2017-05-15 17:07:58 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// 由于babel 默认不处理此文件，所以不使用 ES6 的特性
+
+var Comment=require('./dist/comment').Comment;
+
+// 暂时不想强制使用webpack打包css，所以注释掉之
+// require('./dist/style.css');
+
+
+module.exports={
+    Comment:Comment,
+};
