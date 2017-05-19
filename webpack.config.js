cd5e55c367024e47a20d0a7799152974487e510d
newbienewbie <webbot@sina.com>
2017-05-19 19:08:27 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path=require("path");
+
+const PATHS={
+    app:path.join(__dirname,"demo","src"),
+    build:path.join(__dirname,"demo","dist","js"),
+};
+
+module.exports={
+    entry:{
+        main:path.join(PATHS.app,"index.js"),
+    },
+    output:{
+        path:PATHS.build,
+        filename:"[name].js",
+    },
+    module:{
+        loaders:[
+            {
+                test:/\.jsx?/,
+                loaders:["babel-loader"],//自右向左依次加载
+                include:PATHS.app,
+            },
+            {
+                test:/\.css$/,
+                loaders: ["style-loader","css-loader", ]
+            },
+            {
+                test:/\.less$/,
+                loaders: ["style-loader","css-loader??importLoaders=1",'postcss-loader',"less-loader"]
+            },
+        ],
+    }
+}
