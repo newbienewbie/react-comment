f41796ec4a74713c95b41a5e7f2a44ac3452690d
newbienewbie <webbot@sina.com>
2017-08-08 00:08:49 +0800

    暴露comment组件

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 由于babel 默认不处理此文件，所以不使用 ES6 的特性
 
-var Comment=require('./dist/comment').Comment;
+var Comment=require('./dist/container/comment').Comment;
 
 // 暂时不想强制使用webpack打包css，所以注释掉之
 // require('./dist/style.css');
