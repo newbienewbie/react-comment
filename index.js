47c53072c869c41c9d2d1f41cdf7d88b3907d2d2
newbienewbie <webbot@sina.com>
2017-08-08 11:08:45 +0800

    导出预置的reducer和store

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // 由于babel 默认不处理此文件，所以不使用 ES6 的特性
 
 var Comment=require('./dist/container/comment').Comment;
+var store=require('./dist/store').store;
+var reducers=require('./dist/reducers');
 
 // 暂时不想强制使用webpack打包css，所以注释掉之
 // require('./dist/style.css');
@@ -8,4 +10,8 @@ var Comment=require('./dist/container/comment').Comment;
 
 module.exports={
     Comment:Comment,
+    store,
+    reducer:reducers.reducer,
+    commentReducer:reducers.commentReducer,
+    replyReducer:reducers.replyReducer,
 };
