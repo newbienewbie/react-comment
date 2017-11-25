edf3a5a37917c21eba00196dbe2369101a35b7e0
newbienewbie <webbot@sina.com>
2017-11-25 15:53:26 +0800

    添加登陆遮罩层

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {Comment,store} from '../../index';
+import {Comment,LoginMaskLayer,store} from '../../index';
 import '../../dist/style.css';
 
 
 const scope= document.getElementById('react-comment-container').getAttribute("data-scope");
 const topicId=document.getElementById('react-comment-container').getAttribute("data-topicId");
 
+// window.isLogin=false;
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Comment scope={scope} topicId={topicId} />
-    </Provider>,
+    (<div style={{position:'relative'}}>
+        <LoginMaskLayer isLogin={!!window.isLogin} />
+        <Provider store={store}>
+            <Comment scope={scope} topicId={topicId} />
+        </Provider>
+    </div>),
     document.getElementById("react-comment-container")
 );
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // 由于babel 默认不处理此文件，所以不使用 ES6 的特性
 
 var Comment=require('./dist/container/comment').Comment;
+var LoginMaskLayer=require('./dist/components/login-mask-layer').LoginMaskLayer;
 var store=require('./dist/store').store;
 var reducers=require('./dist/reducers');
 
@@ -10,6 +11,7 @@ var reducers=require('./dist/reducers');
 
 module.exports={
     Comment:Comment,
+    LoginMaskLayer:LoginMaskLayer,
     store,
     reducer:reducers.reducer,
     commentReducer:reducers.commentReducer,
